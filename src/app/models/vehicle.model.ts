05da8fc50e6f170f92bba0300486706cc3528334
marjel <marjel73@gmail.com>
2025-06-22 15:53:26 +0200

    initial commit

---
+++ src/app/models/vehicle.model.ts
@@ -0,0 +1,18 @@
+export interface Vehicle {
+  vendorName: string;
+  vendorCode: string;
+  status: string;
+  makeModel: string;
+  pictureUrl: string;
+  airConditionInd: boolean;
+  transmissionType: string;
+  fuelType: string;
+  driveType: string;
+  passengerQuantity: string;
+  baggageQuantity: string;
+  doorCount: string;
+  vehicleCode: string;
+  rateTotalAmount: number;
+  estimatedTotalAmount: number;
+  currencyCode: string;
+}
